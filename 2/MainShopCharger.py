d06d7872f911660ebca9ede915f856338b9ad6da
kuda-12114 <okuda.tomotomo@gmail.com>
2019-01-08 15:04:04 +0900

    [add] program

--- 2/MainShopCharger.py
+++ 2/MainShopCharger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from StudentCard import StudentCard
+import datetime
 
 
 class MainShopCharger():
+    ChargeDate = "NoCharge"
 
     def insertStudentCard(self, num):
         self.insertedStudentCard = StudentCard.StudentCardList[num]
@@ -12,26 +14,56 @@ class MainShopCharger():
         if self.insertedStudentCard is None:
             print("学生証が挿入されていません")
         else:
+            self.setChargeDate()
             self.insertedStudentCard.setter(
                 self.insertedStudentCard.getter() + money)
             self.printAccountBalance()
+            self.printChargeDate()
 
     def printAccountBalance(self):
         print(self.insertedStudentCard.studentname)
         print(str(self.insertedStudentCard.accountBalance))
 
+    def setChargeDate(self):
+        self.ChargeDate = datetime.datetime.now()
+
+    def printChargeDate(self):
+        print(self.ChargeDate)
+
+    def printSimWord(self):
+        if self.insertedStudentCard is None:
+            print("学生証が挿入されていません")
+        else:
+            print("出身地に関係する単語を表示します")
+            print("!注意! 環境によってはメモリ不足でエラーが起きますのでご注意ください")
+            self.insertedStudentCard.simPrint()
+
     def main(self):
         self.insertedStudentCard = None
-        StudentCard1 = StudentCard(0, "tut")
-        StudentCard2 = StudentCard(1, "tenpaku")
-        # 1枚目へ処理
+        StudentCard1 = StudentCard(0, "tut", "Osaka")
+        StudentCard2 = StudentCard(1, "tenpaku", "Osaka")
+        print("機能を選んでください")
+        print("0 : チャージ機能")
+        print("1 : 出身地の類似語を表示")
+        print("2 : 出身地の天気を表示")
+        mode = input()
         self.insertStudentCard(0)
-        self.chargeMoney(1000)
-        self.chargeMoney(-300)
-        # 2枚目へ処理
-        self.insertStudentCard(1)
-        self.chargeMoney(500)
-        self.chargeMoney(-1000)
+        if mode == '0':
+            # 1枚目へ処理
+            self.chargeMoney(1000)
+            self.chargeMoney(-300)
+            # 2枚目へ処理
+            self.insertStudentCard(1)
+            self.chargeMoney(500)
+            self.chargeMoney(-1000)
+        elif mode == '1':
+            # 類似語の表示
+            self.printSimWord()
+        elif mode == '2':
+            # 出身地の天気を表示
+            self.insertedStudentCard.getWeather()
+        else:
+            print("入力エラー")
 
 
 if __name__ == '__main__':
--- 2/StudentCard.py
+++ 2/StudentCard.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import cv2
+import requests
+import json
+from gensim.models import KeyedVectors
 
 
 class StudentCard():
 
     StudentCardList = []
+
+    Img = cv2.imread("Image.png", 0)
 
     def getter(self):
         return self.accountBalance
@@ -12,8 +18,49 @@ class StudentCard():
     def setter(self, n):
         self.accountBalance = n
 
-    def __init__(self, a, b):
+    def simPrint(self):
+        model_dir = 'entity_vector.model.bin'
+        model = KeyedVectors.load_word2vec_format(model_dir, binary=True)
+        similar_rugby_list = model.most_similar(self.freetext)
+        for similar_set in similar_rugby_list:
+            print(similar_set)
+
+    # 温度変換(ケルビン→摂氏)
+    def k2c(sekf, k):
+        return k - 273.15
+
+    def getWeather(self):
+        print("出身地の天気を表示します")
+        print("インターネットに接続されていない場合エラーが起きますのでご注意ください")
+        # APIキーの指定
+        apikey = "cf133c6636d1768ba2283e0943623008"
+        # 天気を調べたい都市の一覧
+        cities = [self.freetext]
+        # APIのひな型
+        api = "http://api.openweathermap.org/data"
+        api += "/2.5/weather?q={city}&APPID={key}"
+        # 各都市の温度を取得する
+        for name in cities:
+            # APIのURLを得る
+            url = api.format(city=name, key=apikey)
+            # 実際にAPIにリクエストを送信して結果を取得する
+            r = requests.get(url)
+            # 結果はJSON形式なのでデコードする
+            data = json.loads(r.text)
+            # 結果を出力
+            print("+ 都市=", data["name"])
+            print("| 天気=", data["weather"][0]["description"])
+            print("| 最低気温=", self.k2c(data["main"]["temp_min"]))
+            print("| 最高気温=", self.k2c(data["main"]["temp_max"]))
+            print("| 湿度=", data["main"]["humidity"])
+            print("| 気圧=", data["main"]["pressure"])
+            print("| 風向き=", data["wind"]["deg"])
+            print("| 風速度=", data["wind"]["speed"])
+            print("")
+
+    def __init__(self, a, b, c):
         self.studentnum = a
         self.studentname = b
         self.accountBalance = 0
+        self.freetext = c
         self.StudentCardList.append(self)
